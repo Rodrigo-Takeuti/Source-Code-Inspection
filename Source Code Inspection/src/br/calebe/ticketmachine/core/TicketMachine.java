04b6a5b9d5c91402587727a893ffc420b562ae92
41425979 <41425979@099066-P31S202.macklabs.br>
2016-03-28 08:46:05 -0300

    Criação de um objeto do tipo Troco no TicketMachine

--- Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/TicketMachine.java
@@ -13,6 +13,7 @@ public class TicketMachine {
     protected int valor;
     protected int saldo;
     protected int[] papelMoeda = {2, 5, 10, 20, 50, 100};
+    protected Troco troco;
 
     public TicketMachine(int valor) {
         this.valor = valor;
